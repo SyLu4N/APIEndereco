6400ac14bcc5daefb9194558b18b754ff0fe6069
Luan Simões <luaan.carlos@hotmail.com>
2022-05-06 00:30:46 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,65 @@
-import React from 'react';
-import { Provider } from 'react-redux';
+import React, { useState } from 'react';
+import './styles/App.css';
 
-import Routes from './routes';
-import store from './store';
+import foto1 from './images/location.svg';
+import { Loading } from './components/Loading';
+import { Result } from './components/Result';
+import { Form } from './components/Form/Index';
+import { Nav } from './components/nav';
+import { Save } from './components/Save';
 
-function App() {
+export function SearchCEP() {
+  const [cep, setCep] = useState('');
+  const [street, setStreet] = useState('');
+  const [district, setDistrict] = useState('');
+  const [city, setCity] = useState('');
+  const [region, setRegion] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isResult, setIsResult] = useState(false);
+
   return (
-    <Provider store={store}>
-      <Routes />{' '}
-    </Provider>
+    <>
+      <header>
+        <Nav />
+
+        <h1>CONSULTA CEP</h1>
+        <p className="max text p">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum
+          delectus corporis sed assumenda velit blanditiis iste at, mollitia
+        </p>
+        <div className="image">
+          {<img src={foto1} alt="" />}
+          {/*           <div>{isResult ? <Save street={street} cep={cep} /> : ''}</div>
+           */}{' '}
+        </div>
+      </header>
+
+      <Form
+        isLoading={isLoading}
+        cep={cep}
+        setCep={setCep}
+        setStreet={setStreet}
+        setDistrict={setDistrict}
+        setCity={setCity}
+        setRegion={setRegion}
+        setIsLoading={setIsLoading}
+        setIsResult={setIsResult}
+      />
+
+      <main>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <Result
+            cep={cep}
+            street={street}
+            region={region}
+            city={city}
+            district={district}
+            isResult={isResult}
+          />
+        )}
+      </main>
+    </>
   );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './App';
+import { SearchCEP } from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -9,6 +9,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <SearchCEP />
   </React.StrictMode>,
 );
